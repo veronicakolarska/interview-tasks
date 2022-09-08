61bcd44dc27722034333242600e88976169e26b1
veronicakolarska <veronica.kolarska@gmail.com>
2022-09-08 20:44:51 +0300

    feat: added db context logic

--- ConsoleTasks/OffersDatabase/Data/OffersDbContext.cs
+++ ConsoleTasks/OffersDatabase/Data/OffersDbContext.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using OffersDatabase.Models;
 
 namespace OffersDatabase.Data
 {
-    internal class OffersDbContext
+    public class OffersDbContext : DbContext
     {
+        public DbSet<Offer> Offers { get; set; }
+
+        public string DbPath { get; }
+
+        public OffersDbContext()
+        {
+            var folder = Environment.SpecialFolder.DesktopDirectory;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = Path.Join(path, "offers.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
     }
 }
---
+++ ConsoleTasks/OffersDatabase/Migrations/20220908174320_InitialDbCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OffersDatabase.Migrations
+{
+    public partial class InitialDbCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Offers",
+                columns: table => new
+                {
+                    OfferId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MonthlyFee = table.Column<decimal>(type: "TEXT", nullable: false),
+                    NewContractsForMonth = table.Column<int>(type: "INTEGER", nullable: false),
+                    SameContractsForMonth = table.Column<int>(type: "INTEGER", nullable: false),
+                    CancelledContractsForMonth = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Offers", x => x.OfferId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Offers");
+        }
+    }
+}
