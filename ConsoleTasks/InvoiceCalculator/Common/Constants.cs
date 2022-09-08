ada9631b7cd5977492c2d75f3b0496c69fbef4a2
veronicakolarska <veronica.kolarska@gmail.com>
2022-09-08 19:57:34 +0300

    feat: Moved taxes to constants

--- ConsoleTasks/InvoiceCalculator/Common/Constants.cs
+++ ConsoleTasks/InvoiceCalculator/Common/Constants.cs
@@ -14,6 +14,11 @@
         public const decimal MONTHLY_MESSAGES_SENT_SMS_BELOW = 0.18m;
         public const decimal MONTHLY_MESSAGES_SENT_SMS_ABOVE = 0.16m;
         public const decimal MONTHLY_MESSAGES_SENT_SMS = 0.11m;
+        public const decimal MONTHLY_MESSAGES_SENT_MMS_BELOW = 0.25m;
+        public const decimal MONTHLY_MESSAGES_SENT_MMS_ABOVE = 0.23m;
+        public const decimal MONTHLY_MESSAGES_SENT_MMS = 0.18m;
+        public const decimal MONTHLY_MESSAGES_SENT_MIN_RANGE = 50;
+        public const decimal MONTHLY_MESSAGES_SENT_MAX_RANGE = 100;
 
     }
 }
--- ConsoleTasks/InvoiceCalculator/Models/MonthlyMessagesData.cs
+++ ConsoleTasks/InvoiceCalculator/Models/MonthlyMessagesData.cs
@@ -16,12 +16,12 @@ namespace InvoiceCalculator.Models
 
         private decimal CalculateMonthlySmsPrice()
         {
-            if (this.SentSmsCount < 50)
+            if (this.SentSmsCount < Constants.MONTHLY_MESSAGES_SENT_MIN_RANGE)
             {
                 return this.SentSmsCount * Constants.MONTHLY_MESSAGES_SENT_SMS_BELOW;
             }
 
-            if (this.SentSmsCount >= 50 && this.SentSmsCount <= 100)
+            if (this.SentSmsCount >= Constants.MONTHLY_MESSAGES_SENT_MIN_RANGE && this.SentSmsCount <= Constants.MONTHLY_MESSAGES_SENT_MAX_RANGE)
             {
                 return this.SentSmsCount * Constants.MONTHLY_MESSAGES_SENT_SMS_ABOVE;
             }
@@ -31,18 +31,17 @@ namespace InvoiceCalculator.Models
 
         private decimal CalculateMonthlyMmsPrice()
         {
-            if (this.SentMmsCount < 50)
+            if (this.SentMmsCount < Constants.MONTHLY_MESSAGES_SENT_MIN_RANGE)
             {
-                return this.SentMmsCount * 0.25m;
+                return this.SentMmsCount * Constants.MONTHLY_MESSAGES_SENT_MMS_BELOW;
             }
 
-            if (this.SentMmsCount >= 50 && this.SentMmsCount <= 100)
+            if (this.SentMmsCount >= Constants.MONTHLY_MESSAGES_SENT_MIN_RANGE && this.SentMmsCount <= Constants.MONTHLY_MESSAGES_SENT_MAX_RANGE)
             {
-                return this.SentMmsCount * 0.23m;
+                return this.SentMmsCount * Constants.MONTHLY_MESSAGES_SENT_MMS_ABOVE;
             }
 
-            return this.SentMmsCount * 0.18m;
+            return this.SentMmsCount * Constants.MONTHLY_MESSAGES_SENT_MMS;
         }
-
     }
 }
